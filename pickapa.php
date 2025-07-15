c8072b9c16a9d4a7d6fe08c9bd383eb78f9b768c
Ajyol Dhamala <114612254+Ajyol@users.noreply.github.com>
2025-07-15 14:09:40 -0500

    link update for php files

--- pickapa.php
+++ pickapa.php
@@ -5,16 +5,17 @@
 <!--[if gt IE 8]><!-->
 <html class="">
 <!--<![endif]-->
+
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>The Pick: APA Writing Style</title>
-<link href="boilerplate.css" rel="stylesheet" type="text/css">
-<link href="pick59.css" rel="stylesheet" type="text/css">
-<style type="text/css">
-@import url("pick2.css");
-</style>
-<!-- 
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>The Pick: APA Writing Style</title>
+  <link href="boilerplate.css" rel="stylesheet" type="text/css">
+  <link href="pick59.css" rel="stylesheet" type="text/css">
+  <style type="text/css">
+    @import url("pick2.css");
+  </style>
+  <!-- 
 To learn more about the conditional comments around the html tags at the top of the file:
 paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/
 
@@ -24,20 +25,27 @@ Do the following if you're using your customized build of modernizr (http://www.
 * add the "no-js" class to the html tags at the top
 * you can also remove the link to respond.min.js if you included the MQ Polyfill in your modernizr build 
 -->
-<!--[if lt IE 9]>
+  <!--[if lt IE 9]>
 <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 <![endif]-->
-<script src="respond.min.js"></script>
+  <script src="respond.min.js"></script>
 </head>
+
 <body>
-<div class="gridContainer clearfix">
-  <div id="header"><img src="ThePickCanvasHeader2.png" width="100%"></div>
-  <div id="menu"><center><b>
-  <p style="font-family:didot; font-size:22px;padding:0px 5px 0px 5px;"><a href="http://english.selu.edu/pick/Test/pick1.html">Home</a>&nbsp;|&nbsp;<a href="http://english.selu.edu/pick/Test/pickaboutus.html">About Us</a>&nbsp;|&nbsp;<a href="http://english.selu.edu/pick/Test/pickarchiveshome.html">Archive</a>&nbsp;|<a href="http://english.selu.edu/pick/Test/pickresource.html">&nbsp;Resources</a>&nbsp;|&nbsp;<a href="http://english.selu.edu/pick/Test/pickarchiveindex.html">Index</a>&nbsp;|&nbsp;<a href="http://english.selu.edu/pick/Test/picksubmissions.html">Submit</a></p></b></center></div>
-  <div id="apaDiv"><?php include('pickapa.inc.php'); ?></div>
-  <div id="footer"><center><a href="http://www.southeastern.edu/acad_research/programs/writing_center/index.html"><img src="swclogo2.png" width="163" height="40" hspace="5" vspace="5"></a>&nbsp;&nbsp;
-    <a href="http://www.southeastern.edu"><img src="slogo222.png" width="41" height="41" hspace="5" vspace="5"></a>&nbsp;&nbsp;<a href="http://english.selu.edu/manchacreview"><img src="manchaclogo1.png" width="141" height="40" hspace="5" vspace="5"></a>&nbsp;&nbsp;&nbsp;<a href="http://english.selu.edu/pick/Test/pick1.html">Home</a>|<a href="http://english.selu.edu/pick/Test/pickarchiveshome.html">Archive</a>|<a href="http://english.selu.edu/pick/Test/pickarchiveindex.html">Index</a>|<a href="http://english.selu.edu/pick/Test/pickresource.html">Resources</a>|<a href="http://english.selu.edu/pick/Test/picksubmissions.html">Submit</a>|<a href="http://english.selu.edu/pick/Test/pickaboutus.html">About Us</a>
-  </center></div>
-</div>
+  <div class="gridContainer clearfix">
+    <div id="header"><img src="ThePickCanvasHeader2.png" width="100%"></div>
+    <div id="menu">
+      <center><b>
+          <p style="font-family:didot; font-size:22px;padding:0px 5px 0px 5px;"><a href="/pick1.html">Home</a>&nbsp;|&nbsp;<a href="/pickaboutus.html">About Us</a>&nbsp;|&nbsp;<a href="/pickarchiveshome.html">Archive</a>&nbsp;|<a href="/pickresource.html">&nbsp;Resources</a>&nbsp;|&nbsp;<a href="/pickarchiveindex.html">Index</a>&nbsp;|&nbsp;<a href="/picksubmissions.html">Submit</a></p>
+        </b></center>
+    </div>
+    <div id="apaDiv"><?php include('pickapa.inc.php'); ?></div>
+    <div id="footer">
+      <center><a href="http://www.southeastern.edu/acad_research/programs/writing_center/index.html"><img src="swclogo2.png" width="163" height="40" hspace="5" vspace="5"></a>&nbsp;&nbsp;
+        <a href="http://www.southeastern.edu"><img src="slogo222.png" width="41" height="41" hspace="5" vspace="5"></a>&nbsp;&nbsp;<a href="http://english.selu.edu/manchacreview"><img src="manchaclogo1.png" width="141" height="40" hspace="5" vspace="5"></a>&nbsp;&nbsp;&nbsp;<a href="/pick1.html">Home</a>|<a href="/pickarchiveshome.html">Archive</a>|<a href="/pickarchiveindex.html">Index</a>|<a href="/pickresource.html">Resources</a>|<a href="/picksubmissions.html">Submit</a>|<a href="/pickaboutus.html">About Us</a>
+      </center>
+    </div>
+  </div>
 </body>
+
 </html>
--- pickeditorialstaff.php
+++ pickeditorialstaff.php
@@ -5,22 +5,26 @@
 <!--[if gt IE 8]><!-->
 <html class="">
 <!--<![endif]-->
-<head><meta name="keywords" content="Pick, southeastern, Louisiana, university, SELU, SLU, Hammond, journal, publication, required, classes, curriculum, curricula, graduate, undergraduate, model, essay, paper, dissertation, work, English, department, exemplary, peer, faculty, review, student, authored, written, composed, archive, literary, literature, analysis, argumentative, ethnography, exam, non-fiction, descriptive, drama, dialogue, ethical, dilemma, film, free writing, scene, life, narrative, memoir, oral, poetry, proposal, research,  resources, writing, style, stylesheet, format, convention, MLA, APA, CMS, American, psychological, association, Chicago, press, modern, language, manual, guide, scholarly, publishing, OWL, Perdue, lab, center, handout, composition, tutor, consultation, workshop, career, develop, edit, academic, press, submission, submit, form, edition, volume, 38, 39, 41, 42, 43, 44, 45, class, 654, 101, 102, 121, honors, 231, 232, 291, 396, 415, 436, 485, 527, 577, 585, 606, 610, 620, 630, 770, 3701, 498, history, 498, 311, math, 367, philosophy, common, read, social"><meta name="description" content="The Pick is Southeastern’s journal of student-authored works, representing all disciplines in the university’s curriculum.&quot;&gt;
-&lt;meta name=&quot;copyright&quot; content=&quot;The Pick, Southeastern Louisiana University's Journal of student-authored print works"><meta name="Classification" content="Non-Profit Literary Journal">
-<meta name="category" content="University Literary Journal">
-<meta name="author" content="The Pick Editor, thepick@southeastern.edu, (985) 549-2076.">
-<meta name="medium" content="print journal, online journal">
-<meta name="Distribution" content="Global">
-<meta name="Robots" content="index,follow">
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>The Pick Editorial Staff</title>
-<link href="boilerplate.css" rel="stylesheet" type="text/css">
-<link href="pick11.css" rel="stylesheet" type="text/css">
-<style type="text/css">
-@import url("pick2.css");
-</style>
-<!-- 
+
+<head>
+  <meta name="keywords" content="Pick, southeastern, Louisiana, university, SELU, SLU, Hammond, journal, publication, required, classes, curriculum, curricula, graduate, undergraduate, model, essay, paper, dissertation, work, English, department, exemplary, peer, faculty, review, student, authored, written, composed, archive, literary, literature, analysis, argumentative, ethnography, exam, non-fiction, descriptive, drama, dialogue, ethical, dilemma, film, free writing, scene, life, narrative, memoir, oral, poetry, proposal, research,  resources, writing, style, stylesheet, format, convention, MLA, APA, CMS, American, psychological, association, Chicago, press, modern, language, manual, guide, scholarly, publishing, OWL, Perdue, lab, center, handout, composition, tutor, consultation, workshop, career, develop, edit, academic, press, submission, submit, form, edition, volume, 38, 39, 41, 42, 43, 44, 45, class, 654, 101, 102, 121, honors, 231, 232, 291, 396, 415, 436, 485, 527, 577, 585, 606, 610, 620, 630, 770, 3701, 498, history, 498, 311, math, 367, philosophy, common, read, social">
+  <meta name="description" content="The Pick is Southeastern’s journal of student-authored works, representing all disciplines in the university’s curriculum.&quot;&gt;
+&lt;meta name=&quot;copyright&quot; content=&quot;The Pick, Southeastern Louisiana University's Journal of student-authored print works">
+  <meta name="Classification" content="Non-Profit Literary Journal">
+  <meta name="category" content="University Literary Journal">
+  <meta name="author" content="The Pick Editor, thepick@southeastern.edu, (985) 549-2076.">
+  <meta name="medium" content="print journal, online journal">
+  <meta name="Distribution" content="Global">
+  <meta name="Robots" content="index,follow">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>The Pick Editorial Staff</title>
+  <link href="boilerplate.css" rel="stylesheet" type="text/css">
+  <link href="pick11.css" rel="stylesheet" type="text/css">
+  <style type="text/css">
+    @import url("pick2.css");
+  </style>
+  <!-- 
 To learn more about the conditional comments around the html tags at the top of the file:
 paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/
 
@@ -30,21 +34,28 @@ Do the following if you're using your customized build of modernizr (http://www.
 * add the "no-js" class to the html tags at the top
 * you can also remove the link to respond.min.js if you included the MQ Polyfill in your modernizr build 
 -->
-<!--[if lt IE 9]>
+  <!--[if lt IE 9]>
 <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 <![endif]-->
-<script src="respond.min.js"></script>
+  <script src="respond.min.js"></script>
 </head>
+
 <body>
-<div class="gridContainer clearfix">
-  <div id="header"><img src="ThePickCanvasHeaderFinal.png" width="100%"></div>
-  <div id="menu"><center><b>
-  <p style="font-family:didot; font-size:22px;padding:0px 5px 0px 5px;"><a href="http://english.selu.edu/pick/Test/pick1.html">Home</a>&nbsp;|&nbsp;<a href="http://english.selu.edu/pick/Test/pickaboutus.html">About Us</a>&nbsp;|&nbsp;<a href="http://english.selu.edu/pick/Test/pickarchiveshome.html">Archive</a>&nbsp;|<a href="http://english.selu.edu/pick/Test/pickresource.html">&nbsp;Resources</a>&nbsp;|&nbsp;<a href="http://english.selu.edu/pick/Test/pickarchiveindex.html">Index</a>&nbsp;|&nbsp;<a href="http://english.selu.edu/pick/Test/picksubmissions.html">Submit</a></p></b></center></div>
-  <div id="editorialStaffDiv1"><?php include('pickeditorialstaff.inc.php'); ?></div>
-  
-  <div id="footer"><center><a href="http://www.southeastern.edu/acad_research/programs/writing_center/index.html"><img src="swclogo2.png" width="163" height="40" hspace="5" vspace="5"></a>&nbsp;&nbsp;
-    <a href="http://www.southeastern.edu"><img src="slogo222.png" width="41" height="41" hspace="5" vspace="5"></a>&nbsp;&nbsp;<a href="http://english.selu.edu/manchacreview"><img src="manchaclogo1.png" width="141" height="40" hspace="5" vspace="5"></a>&nbsp;&nbsp;&nbsp;<a href="http://english.selu.edu/pick/Test/pick1.html">Home</a>|<a href="http://english.selu.edu/pick/Test/pickarchiveshome.html">Archive</a>|<a href="http://english.selu.edu/pick/Test/pickarchiveindex.html">Index</a>|<a href="http://english.selu.edu/pick/Test/pickresource.html">Resources</a>|<a href="http://english.selu.edu/pick/Test/picksubmissions.html">Submit</a>|<a href="http://english.selu.edu/pick/Test/pickaboutus.html">About Us</a>
-  </center></div>
-</div>
+  <div class="gridContainer clearfix">
+    <div id="header"><img src="ThePickCanvasHeaderFinal.png" width="100%"></div>
+    <div id="menu">
+      <center><b>
+          <p style="font-family:didot; font-size:22px;padding:0px 5px 0px 5px;"><a href="/pick1.html">Home</a>&nbsp;|&nbsp;<a href="/pickaboutus.html">About Us</a>&nbsp;|&nbsp;<a href="/pickarchiveshome.html">Archive</a>&nbsp;|<a href="/pickresource.html">&nbsp;Resources</a>&nbsp;|&nbsp;<a href="/pickarchiveindex.html">Index</a>&nbsp;|&nbsp;<a href="/picksubmissions.html">Submit</a></p>
+        </b></center>
+    </div>
+    <div id="editorialStaffDiv1"><?php include('pickeditorialstaff.inc.php'); ?></div>
+
+    <div id="footer">
+      <center><a href="http://www.southeastern.edu/acad_research/programs/writing_center/index.html"><img src="swclogo2.png" width="163" height="40" hspace="5" vspace="5"></a>&nbsp;&nbsp;
+        <a href="http://www.southeastern.edu"><img src="slogo222.png" width="41" height="41" hspace="5" vspace="5"></a>&nbsp;&nbsp;<a href="http://english.selu.edu/manchacreview"><img src="manchaclogo1.png" width="141" height="40" hspace="5" vspace="5"></a>&nbsp;&nbsp;&nbsp;<a href="/pick1.html">Home</a>|<a href="/pickarchiveshome.html">Archive</a>|<a href="/pickarchiveindex.html">Index</a>|<a href="/pickresource.html">Resources</a>|<a href="/picksubmissions.html">Submit</a>|<a href="/pickaboutus.html">About Us</a>
+      </center>
+    </div>
+  </div>
 </body>
+
 </html>
